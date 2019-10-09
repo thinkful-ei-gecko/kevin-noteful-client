086e62b422b65b8d9bb2c7d4564671a835bd8561
Kevin Wei <chenghaowei@gmail.com>
2019-10-09 10:56:35 -0400

    add notes validation and folderId dropdown

--- src/AddNote.js
+++ src/AddNote.js
@@ -16,29 +16,38 @@ class AddNote extends Component {
 
   state = {
     error: null,
-    note_name: '',
-    content: '',
-    folder_id: 1,
+    note_name: {
+      value: '',
+      touched: false,
+    },
+    content: {
+      value: '',
+      touched: false,
+    },
+    folder_id: {
+      value: null,
+      touched: false,
+    },
   };
 
   handleChangeNoteName = (e) => {
-    this.setState({ note_name: e.target.value });
+    this.setState({ note_name: { value: e.target.value, touched: true } });
   };
 
   handleChangeNoteContent = (e) => {
-    this.setState({ content: e.target.value });
+    this.setState({ content: { value: e.target.value, touched: true } });
   };
 
   handleChangeNoteFolder = (e) => {
-    this.setState({ folder_id: Number(e.target.value) });
-  }
+    this.setState({ folder_id: { value: Number(e.target.value), touched: true } });
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
     const newNote = {
-      note_name: this.state.note_name,
-      content: this.state.content,
-      folder_id: this.state.folder_id,
+      note_name: this.state.note_name.value,
+      content: this.state.content.value,
+      folder_id: this.state.folder_id.value,
     };
     this.setState({ error: null });
     fetch(`${config.API_ENDPOINT}/notes`, {
@@ -61,8 +70,31 @@ class AddNote extends Component {
       });
   };
 
+  validateName() {
+    const name = this.state.note_name.value.trim();
+    if (name.length === 0) {
+      return 'Name is required';
+    }
+  }
+
+  validateContent() {
+    const content = this.state.content.value.trim();
+    if (content.length === 0) {
+      return 'Content is required';
+    }
+  }
+
+  validateFolderId() {
+    const folder = this.state.folder_id.value;
+    if (!folder) {
+      return 'Must specify an existing folder';
+    }
+  }
+
   render() {
     const { error } = this.state;
+    const { folders = [] } = this.context;
+
     return (
       <section className="AddNote">
         <CircleButton
@@ -77,19 +109,54 @@ class AddNote extends Component {
         </CircleButton>
         <h2>Add Note</h2>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="noteName">Note Name</label><br/>
-          <input name="noteName" id="noteName" onChange={this.handleChangeNoteName} /><br/><br/>
+          <label htmlFor="noteName">Note Name</label>
+          <br />
+          <input name="noteName" id="noteName" onChange={this.handleChangeNoteName} />
+          {this.state.note_name.touched && (
+            <div className="error">{this.validateName()}</div>
+          )}
+          <br />
+          <br />
 
-          <label htmlFor="folderid">Folder ID</label><br/>
-          <input name="folderid" id="folderid" onChange={this.handleChangeNoteFolder} /><br/><br/>
+          <label htmlFor="folderid">Folder ID</label>
+          <br />
+          <select name="folderid" id="folderid" onChange={this.handleChangeNoteFolder}>
+            <option value={''}>Select Folder</option>
+            {folders.map((folder, index) => {
+              return (
+                <option value={folder.id} key={index}>
+                  {folder.folder_name}
+                </option>
+              );
+            })}
+          </select>
+          {this.state.folder_id.touched && (
+            <div className="error">{this.validateFolderId()}</div>
+          )}
+          <br />
+          <br />
 
-          <label htmlFor="noteContent">Content</label><br/>
+          <label htmlFor="noteContent">Content</label>
+          <br />
           <textarea
             name="noteContent"
             id="noteContent"
             onChange={this.handleChangeNoteContent}
-          /><br/><br/>
-          <button type="submit">Add Note</button>
+          />
+          {this.state.content.touched && (
+            <div className="error">{this.validateContent()}</div>
+          )}
+          <br />
+          <br />
+
+          <button
+            type="submit"
+            disabled={
+              this.validateName() || this.validateFolderId() || this.validateContent()
+            }
+          >
+            Add Note
+          </button>
         </form>
       </section>
     );
